5879ee55dad05d4ec6c97179ffd943f6ea4e842d
Ping <ping@nextacademy.com>
2016-05-11 00:40:12 -0400

    Add account_spec.rb file

---
+++ account_spec.rb
@@ -0,0 +1,29 @@
+require "rspec"
+
+require_relative "account"
+
+describe Account do
+  describe "#initialize" do
+
+  end
+
+  describe "#transactions" do
+
+  end
+
+  describe "#balance" do
+
+  end
+
+  describe "#account_number" do
+
+  end
+
+  describe "deposit!" do
+
+  end
+
+  describe "#withdraw!" do
+
+  end
+end
